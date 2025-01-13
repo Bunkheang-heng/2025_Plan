e7368236dd199ce9d727ff23e42f7beefd3ce69b
kheang <bunkheangheng99@gmail.com>
2025-01-14 01:19:40 +0700

    fix the bug

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyC6qiw0TN4-pxtiyV3zDQ1laW-JPnOUPis",
+  authDomain: "kheang158.firebaseapp.com",
+  projectId: "kheang158",
+  storageBucket: "kheang158.firebasestorage.app",
+  messagingSenderId: "913855110057",
+  appId: "1:913855110057:web:f05476597c36dc356a42b8",
+  measurementId: "G-GGDSEKVZK5"
 };
 
 const app = initializeApp(firebaseConfig);
